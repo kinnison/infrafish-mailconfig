47eae852f63487f9267c1b777f343576961ce077
Daniel Silverstone <dsilvers@digital-scurf.org>
2023-07-01 13:18:39 +0100

    chore: Initialise project
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Infrafish Mail Config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        mailconfig = pkgs.rustPlatform.buildRustPackage {
+          pname = "infrafish-mailconfig";
+          version = "git";
+          src = ./.;
+          cargoLock = { lockFile = ./Cargo.lock; };
+
+          nativeBuildInputs = with pkgs; [ postgresql ];
+        };
+      in {
+        packages = {
+          inherit mailconfig;
+          default = mailconfig;
+        };
+        devShell = pkgs.callPackage ./shell.nix { };
+      }));
+}
